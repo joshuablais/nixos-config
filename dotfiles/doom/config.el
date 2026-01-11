d0e4c666ff7c3c13e6a79de2b358d637c81a1a62
Joshua Blais <josh@joshblais.com>
2026-01-11 12:03:41 -0700

    add shell functionality

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -539,85 +539,6 @@
         :localleader
         "a" #'my/archive-done-task))
 
-(defun +calendar/open-calendar ()
-  "Open calfw calendar with org integration."
-  (interactive)
-  (require 'calfw)
-  (require 'calfw-org)
-  
-  ;; Apply Compline faces
-  (custom-set-faces!
-   '(cfw:face-title :foreground "#e0dcd4" :weight bold :height 1.2)
-   '(cfw:face-header :foreground "#b8c4b8" :weight bold)
-   '(cfw:face-sunday :foreground "#cdacac" :weight bold)
-   '(cfw:face-saturday :foreground "#b4c0c8" :weight bold)
-   '(cfw:face-grid :foreground "#282c34")
-   '(cfw:face-today :background "#171a1e" :weight bold)
-   '(cfw:face-select :background "#282c34" :foreground "#f0efeb")
-   '(cfw:face-schedule :foreground "#b8c4b8")
-   '(cfw:face-deadline :foreground "#cdacac"))
-  
-  (calfw-org-open-calendar))
-
-;; Prevent byte-compilation of this function
-(put '+calendar/open-calendar 'byte-compile 'byte-compile-file-form-defmumble)
-
-(after! org
-  (defvar my/contacts-file "~/org/roam/contacts.org")
-  
-  (defun my/contacts-get-emails ()
-    "Extract all emails from contacts.org."
-    (let (contacts)
-      (with-current-buffer (find-file-noselect my/contacts-file)
-        (org-with-wide-buffer
-         (goto-char (point-min))
-         (while (re-search-forward "^\\*+ \\(.+\\)$" nil t)
-           (let ((name (match-string 1))
-                 (email (org-entry-get (point) "EMAIL")))
-             (when email
-               (dolist (addr (split-string email "," t " "))
-                 (push (cons name (string-trim addr)) contacts)))))))
-      (nreverse contacts)))
-  
-  (defun my/contacts-complete ()
-    "Complete email addresses from contacts.org."
-    (let* ((end (point))
-           (start (save-excursion
-                    (skip-chars-backward "^:,; \t\n")
-                    (point)))
-           (contacts (my/contacts-get-emails))
-           (collection (mapcar 
-                       (lambda (contact)
-                         (format "%s <%s>" (car contact) (cdr contact)))
-                       contacts)))
-      (list start end collection :exclusive 'no)))
-  
-  (add-hook 'message-mode-hook
-            (lambda ()
-              (setq-local completion-at-point-functions
-                          (cons 'my/contacts-complete
-                                completion-at-point-functions)))))
-
-(after! mu4e
-  (setq mu4e-compose-complete-addresses nil)
-  
-  (defun my/update-last-contacted ()
-    (when (and (derived-mode-p 'mu4e-compose-mode)
-               mu4e-compose-parent-message)
-      (when-let* ((from (mu4e-message-field mu4e-compose-parent-message :from))
-                  (email (if (stringp from) from (cdar from))))
-        (when (stringp email)
-          (with-current-buffer (find-file-noselect my/contacts-file)
-            (save-excursion
-              (goto-char (point-min))
-              (when (search-forward email nil t)
-                (org-back-to-heading)
-                (org-set-property "LAST_CONTACTED" 
-                                (format-time-string "[%Y-%m-%d %a %H:%M]"))
-                (save-buffer))))))))
-  
-  (add-hook 'mu4e-compose-mode-hook #'my/update-last-contacted))
-
 (use-package! org-roam
   :defer t
   :commands (org-roam-node-find 
@@ -2015,44 +1936,6 @@ This function is designed to be called via `emacsclient -e`."
 ;; Remove EWW from popup rules to make it open in a full buffer
 (after! eww
   (set-popup-rule! "^\\*eww\\*" :ignore t))
-
-(defun jb/clipboard-manager ()
-  "Browse kill ring and copy selection to system clipboard."
-  (interactive)
-  (let* ((candidates (delete-dups kill-ring))
-         (selected (consult--read
-                    candidates
-                    :prompt "Clipboard history: "
-                    :sort nil
-                    :require-match t
-                    :category 'kill-ring
-                    :history 'consult--yank-history)))
-    (when selected
-      (cond
-       ;; Wayland - use wl-copy directly
-       ((executable-find "wl-copy")
-        (with-temp-buffer
-          (insert selected)
-          (call-process-region (point-min) (point-max) "wl-copy")))
-       
-       ;; X11 terminal
-       ((executable-find "xclip")
-        (with-temp-buffer
-          (insert selected)
-          (call-process-region (point-min) (point-max) "xclip" nil nil nil
-                              "-selection" "clipboard")))
-       
-       ;; GUI fallback
-       ((display-graphic-p)
-        (gui-set-selection 'CLIPBOARD selected)))
-      
-      (message "Copied to system clipboard: %s" 
-               (truncate-string-to-width selected 50 nil nil "...")))))
-
-;; Bind it
-(map! :leader
-      (:prefix "y" 
-       :desc "Clipboard manager" "y" #'jb/clipboard-manager))
 
 ;; Universal Launcher
 (load! "lisp/universal-launcher")
