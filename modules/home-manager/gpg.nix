3954ed71986401511154821b433151d5d2d0a08f
Joshua Blais <josh@joshblais.com>
2026-01-31 14:57:36 -0700

    gpg changes

--- modules/home-manager/gpg.nix
+++ modules/home-manager/gpg.nix
@@ -3,18 +3,19 @@
 {
   services.gpg-agent = {
     enable = true;
-    enableSshSupport = true;
-
+    enableSshSupport = false;
     defaultCacheTtl = 86400;
     maxCacheTtl = 86400;
-    defaultCacheTtlSsh = 86400;
-
     pinentry.package = pkgs.pinentry-emacs;
-
     extraConfig = ''
       allow-emacs-pinentry
       allow-loopback-pinentry
     '';
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
   };
 
   programs.zsh.initExtra = ''
