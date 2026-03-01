8fe22ecf51dba4e9d9828b2bf7b8014d3177b6ca
Joshua Blais <josh@joshblais.com>
2026-03-01 13:42:21 -0700

    calendar added

--- dotfiles/doom/custom.el
+++ dotfiles/doom/custom.el
@@ -11,7 +11,7 @@
      "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("~/Development/precipice/README.org"
+   '("~/org/calendar.org" "/home/joshua/Development/precipice/README.org"
      "/home/joshua/Projects/Infrastructure/README.org"
      "/home/joshua/Development/go-creation/README.org"
      "/home/joshua/Projects/writing/README.org" "/home/joshua/org/pomodoro.org"
