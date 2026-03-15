86a5412795e3289b8a9bce195aa6974bf8a45e39
Joshua Blais <josh@joshblais.com>
2026-03-15 15:39:58 -0600

    emacs x11/wayland

--- modules/home-manager/emacs-wayland.nix
+++ modules/home-manager/emacs-wayland.nix
@@ -2,7 +2,7 @@
 {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs; # This is the ONLY emacs installation
+    package = pkgs.emacs-pgtk;
     extraPackages =
       epkgs: with epkgs; [
         doom
@@ -10,35 +10,35 @@
         pdf-tools
         org-roam
         treesit-grammars.with-all-grammars
-        mu4e # Move this from system level
+        mu4e
       ];
   };
-
-  # Add the systemd user service
   systemd.user.services.emacs = {
     Unit = {
       Description = "Emacs daemon";
-      After = [ "graphical-session-pre.target" ];
+      After = [ "graphical-session.target" ];
       PartOf = [ "graphical-session.target" ];
+      Requires = [ "graphical-session.target" ];
     };
-
     Service = {
       Type = "notify";
       ExecStart = "${config.programs.emacs.finalPackage}/bin/emacs --fg-daemon";
       ExecStop = "${config.programs.emacs.finalPackage}/bin/emacsclient --eval '(kill-emacs)'";
       Restart = "on-failure";
-
+      PassEnvironment = [
+        "WAYLAND_DISPLAY"
+        "XDG_RUNTIME_DIR"
+        "DISPLAY"
+      ];
       Environment = [
         "PATH=${config.home.profileDirectory}/bin:/run/current-system/sw/bin"
-        "GDK_BACKEND=x11"
+        "GDK_BACKEND=wayland"
+
       ];
     };
-
     Install.WantedBy = [ "graphical-session.target" ];
   };
-
   home.sessionPath = [ "${config.home.homeDirectory}/.emacs.d/bin" ];
-
   home.sessionVariables = {
     DOOMDIR = "${config.home.homeDirectory}/.config/doom";
     DOOMLOCALDIR = "${config.home.homeDirectory}/.local/share/doom";
