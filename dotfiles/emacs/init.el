5955c7fb39fb13690f45cccab5953bcfabad7d7f
Joshua Blais <josh@joshblais.com>
2026-03-29 14:51:31 -0600

    init cleanup

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1,5 +1,6 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+;; Username setup
 (setq user-full-name "Joshua Blais"
       user-mail-address "josh@joshblais.com")
 (setq auth-sources '("~/.authinfo.gpg" "~/.authinfo")
@@ -51,8 +52,9 @@
 
 (setq use-package-always-defer t
       use-package-always-ensure t
-      use-package-expand-minimally t
-      use-package-compute-statistics t)
+      ;; Testing for package timing
+      ;; use-package-compute-statistics t
+      use-package-expand-minimally t)
 
 ;; no-littering must run before anything writes files
 (setq custom-file (expand-file-name "etc/custom.el" user-emacs-directory))
@@ -64,10 +66,8 @@
         no-littering-var-directory  "~/.local/share/emacs/")
   :config
   (setq auto-save-file-name-transforms
-        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-  (setq backup-directory-alist
-        `((".*" . ,(no-littering-expand-var-file-name "backup/")))))
-(elpaca-wait) ; block until no-littering is fully built and configured
+        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+  (elpaca-wait) ; block until no-littering is fully built and configured
 (when (file-exists-p custom-file)
   (load custom-file))
 
@@ -124,21 +124,19 @@
   :config
   (load-theme 'compline t))
 
-(custom-set-faces
- '(hl-line ((t (:background "#22262b" :foreground unspecified :extend t)))))
-
-;; Opacity
-(add-to-list 'default-frame-alist '(alpha-background . 90))
+(with-eval-after-load 'hl-line
+  (set-face-attribute 'hl-line nil
+                      :background "#22262b"
+                      :foreground 'unspecified
+                      :extend t))
 
 (use-package which-key
-  :defer 1
   :config
   (setq which-key-idle-delay 0.2)
   (which-key-mode 1))
 
 (use-package recentf
   :ensure nil
-  :defer 1
   :config
   (setq recentf-max-menu-items 25
         recentf-max-saved-items 100)
@@ -150,7 +148,6 @@
 
 (use-package savehist
   :ensure nil
-  :defer 1
   :config
   (setq history-length 1000
         history-delete-duplicates t
@@ -162,11 +159,11 @@
                  recentf-list))
     (add-to-list 'savehist-additional-variables var)))
 
+(elpaca-wait)
+
 ;; Modules
 (add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp/custom/" user-emacs-directory))
-
-(elpaca-wait)
 
 (require 'evil-config)
 (require 'dashboard)
