29e81a9b8887e9047e68fc6745f42c13beca071c
Joshua Blais <josh@joshblais.com>
2026-04-03 07:56:33 -0600

    theming

--- modules/desktop/theming.nix
+++ modules/desktop/theming.nix
@@ -6,11 +6,11 @@
     zafiro-icons
     papirus-icon-theme
     polkit_gnome
+    glib
+    gsettings-desktop-schemas
   ];
-
   programs.dconf.enable = true;
   security.polkit.enable = true;
-
   systemd.user.services.polkit-gnome-authentication-agent-1 = {
     description = "polkit-gnome-authentication-agent-1";
     wantedBy = [ "graphical-session.target" ];
@@ -23,5 +23,16 @@
       RestartSec = 1;
       TimeoutStopSec = 10;
     };
+  };
+  environment.sessionVariables = {
+    XDG_DATA_DIRS = [
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+    ];
+  };
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
   };
 }
